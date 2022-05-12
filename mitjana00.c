f4709f88c2394730e5ec116e5b7375033431bf31
Aziz Didouh <aziz.r8@hotmail.com>
2022-05-12 21:39:54 +0200

    Conflicte 1 entre br0 i master

--- mitjana00.c
+++ mitjana00.c
@@ -1,5 +1,5 @@
-//Autor: dc1718daw2
-//Data i hora: 201712011403
+//Autor: Aziz Didouh
+//Data i hora: 15/05/2022
 
 #include <stdio.h>
 
@@ -22,5 +22,6 @@ int main() {
 	}
 	mitjana=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitjana);	
+	printf("Operació finalitzada amb èxit\n");
 	return 0;
 }
